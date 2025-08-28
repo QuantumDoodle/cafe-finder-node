cd1e79e4457065ed6527e74cdd6af2494f30ca1b
Naresh Vishwakarma <NareshVishwakarma02282000@gmail.com>
2025-08-29 03:06:00 +0530

    Display cafes as markers on map

--- public/script.js
+++ public/script.js
@@ -7,4 +7,31 @@ function initMap() {
     center: defaultLocation,
     zoom: 14,
   });
+
+  fetch(`http://localhost:5000/cafes?lat=${center.lat}&lng=${center.lng}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.results) {
+        data.results.forEach((place) => {
+          if (place.geometry && place.geometry.location) {
+            const marker = new google.maps.Marker({
+              position: place.geometry.location,
+              map,
+              title: place.name,
+            });
+
+            const infoWindow = new google.maps.InfoWindow({
+              content: `<h3>${place.name}</h3>
+                        <p>Rating: ${place.rating || "N/A"}</p>
+                        <p>${place.vicinity || ""}</p>`,
+            });
+
+            marker.addListener("click", () => {
+              infoWindow.open(map, marker);
+            });
+          }
+        });
+      }
+    })
+    .catch((err) => console.error("Error fetching cafes:", err));
 }
