7fcbca8e54c9ff2293e2e6e98826e1c30fc75582
Naresh Vishwakarma <NareshVishwakarma02282000@gmail.com>
2025-08-29 02:59:12 +0530

    Step 4: Add /cafes route to fetch nearby cafes from Google Places API

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config()
+const axios = require("axios");
+
 const express = require("express");
+
 const cors = require("cors");
 
 const app = express();
@@ -12,5 +15,21 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.send("Cafe Finder Backend is running 🚀");
 });
+
+app.get("/cafes", async(req,res)=>{
+    try{
+        const { lat , lng } = req.query;
+        const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1500&type=cafe&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+
+        const response = await axios.get(url);
+        res.json(response.data);
+
+    }catch(error){
+        console.error(error); 
+         res.status(500).json({ error: "Something went wrong!" });
+    }
+})
+
+
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
